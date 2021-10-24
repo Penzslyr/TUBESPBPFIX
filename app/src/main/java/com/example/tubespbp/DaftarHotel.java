56a12224428199257c010c50cf46758f00dd818f
Fendi <Fendi.prakoso048@gmail.com>
2021-10-24 23:32:00 +0700

    hampir selesai + kurang rapiin layout

--- app/src/main/java/com/example/tubespbp/DaftarHotel.java
+++ app/src/main/java/com/example/tubespbp/DaftarHotel.java
@@ -24,21 +24,21 @@ public class DaftarHotel {
     public static final DataHotel oyo1= new DataHotel("Hotel Oyo Pasti Bisa", "18",
             "Rp 700.000,00 ", "https://th.bing.com/th/id/OIP.K280yktqIXGZ67FhkMSgdQHaE8?pid=ImgDet&rs=1");
     public static final DataHotel oyo2= new DataHotel("Hotel Nusa Indah", "91",
-            "Rp 500.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 500.000,00 ", "https://th.bing.com/th/id/R.5bdc2edf2757030ccb99c0e3bb99925c?rik=Env8%2fJL%2f%2fAfAuA&riu=http%3a%2f%2fwallpapersdsc.net%2fwp-content%2fuploads%2f2017%2f04%2fHotel-Wallpapers-HQ.jpg&ehk=DlR5KpysRlp8HZmPEfpRmiSZ1qTbCbJ8e6GvbiYBfi0%3d&risl=&pid=ImgRaw&r=0");
     public static final DataHotel oyo3= new DataHotel("Hotel Kambangan", "71",
-            "Rp 400.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 400.000,00 ", "https://th.bing.com/th/id/R.9c750ea09429ff98e50911a4e5f4f11f?rik=39FN39LC2biShg&riu=http%3a%2f%2fyesofcorsa.com%2fwp-content%2fuploads%2f2017%2f04%2fHotels-Wallpaper.jpg&ehk=j8VERPqYO2ixKbbQc%2bBtwbh3qX6YQn8oPBl2cZ1ibYE%3d&risl=1&pid=ImgRaw&r=0");
     public static final DataHotel oyo4= new DataHotel("Hotel Rencana Kita", "12",
-            "Rp 700.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 700.000,00 ", "https://travelmontenegro.me/wp-content/uploads/2018/05/splendid-hotel-becici.jpg");
     public static final DataHotel oyo5= new DataHotel("Hotel Satu Malam", "17",
-            "Rp 450.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 450.000,00 ", "https://www.hotelierindia.com/public/images/2020/11/04/Image-2---Hilton-Goa-Resort_Lounge-Pool.jpg");
     public static final DataHotel oyo6= new DataHotel("Hotel Mana Aja", "18",
-            "Rp 300.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 300.000,00 ", "https://estateintel.com/_a/app/uploads/2020/04/01a_hilton-hotel-lagos3.jpg");
     public static final DataHotel oyo7= new DataHotel("Hotel Indah", "15",
-            "Rp 600.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 600.000,00 ", "https://th.bing.com/th/id/OIP.bioF5YaJM_ufAJrecbIkMQHaEo?pid=ImgDet&w=2560&h=1600&rs=1");
     public static final DataHotel oyo8= new DataHotel("Hotel Negara", "13",
-            "Rp 550.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 550.000,00 ", "https://www.sarajohn.org/wp-content/uploads/2017/01/sharaton.jpg");
     public static final DataHotel oyo9= new DataHotel("Hotel Karantina", "12",
-            "Rp 300.000,00 ", "https://i.ytimg.com/vi/prZBPTfcrbA/maxresdefault.jpg");
+            "Rp 300.000,00 ", "https://vistapointe.net/images/hotel-1.jpg");
 
 
 }
