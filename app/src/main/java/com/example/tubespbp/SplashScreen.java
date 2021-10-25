eaa8de75e7336114ff2649ec77ddcfdbbdfb10a5
Fendi <Fendi.prakoso048@gmail.com>
2021-10-25 20:57:04 +0700

    hampir selesai + kurang rapiin layout + fix push notif

--- app/src/main/java/com/example/tubespbp/SplashScreen.java
+++ app/src/main/java/com/example/tubespbp/SplashScreen.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
     Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
         h.postDelayed(new Runnable() {
             @Override
